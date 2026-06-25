6f096fdcb62b42cf8fc047d623ac5971c6e64abe
MarcusBerggren <borkamannen@gmail.com>
2026-06-25 15:46:57 +0200

    La upp lite mallar

---
+++ js/app.js
@@ -0,0 +1,43 @@
+
+
+// #region ELEMENT
+/* Här hämtar du allt från HTML. */
+
+const button = document.getElementById("x");
+const buttons = document.querySelectorAll("y");
+
+// #endregion
+
+// #region STATE
+/* Vilken information behöver programmet komma ihåg? */
+
+let globalVariable = null;
+
+// #endregion
+
+// #region EVENT LISTENERS
+/* Här kopplar du alla lyssnare. */
+
+button.addEventListener("click", function (inputValue) {
+   console.log(inputValue);
+});
+
+button.addEventListener("click", funcionName());
+// #endregion
+
+// #region FUNCTIONS
+/* Här ligger all logik. */
+
+function functionName(inputValue) {
+   console.log(inputValue);
+}
+
+// #endregion
+
+// #region STARTUP
+/* Kod som ska köras när sidan laddas. */
+
+
+
+// #endregion
+
